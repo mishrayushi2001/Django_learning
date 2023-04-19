88336318b56e5e2998ec0dc2770b17911bb1f726
mishrayushi2001 <mishrayushi2000@gmail.com>
2023-04-19 20:28:24 +0530

    filters

--- gs6/course/views.py
+++ gs6/course/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-
+from datetime import datetime
 def learn_django(request):
     # coursename = {'cname':'Django'}
-    cname = 'Django'
-    duration ='4 Months'
-    seats =10
-    django_details ={'n':cname,'du':duration,'st':seats}
-    return render(request,'course/courseone.html',context=django_details)
+    # cname = 'Django'
+    # duration ='4 Months'
+    # seats =10
+    # django_details ={'n':cname,'du':duration,'st':seats}
+    d= datetime.now
+    return render(request,'course/courseone.html',{'dt':d})
 def learn_python(request):
     return render(request,'course/coursetwo.html')
