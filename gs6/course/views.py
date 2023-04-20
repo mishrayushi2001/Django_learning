a6922d4cdf67e0b3e88d5153cc0b475a2644abff
mishrayushi2001 <mishrayushi2000@gmail.com>
2023-04-20 16:40:17 +0530

    if else tag with filter

--- gs6/course/views.py
+++ gs6/course/views.py
@@ -6,7 +6,7 @@ def learn_django(request):
     # duration ='4 Months'
     # seats =10
     # django_details ={'n':cname,'du':duration,'st':seats}
-    d= datetime.now
-    return render(request,'course/courseone.html',{'dt':d})
+    # d= datetime.now
+    return render(request,'course/courseone.html',{'nm':'Django','st':5})
 def learn_python(request):
     return render(request,'course/coursetwo.html')
