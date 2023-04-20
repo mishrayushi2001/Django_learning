ae8cfe83afd19c9685fa36d39c8a3ce7854e9cab
mishrayushi2001 <mishrayushi2000@gmail.com>
2023-04-20 18:09:01 +0530

    templates

--- gs6/course/views.py
+++ gs6/course/views.py
@@ -7,6 +7,14 @@ def learn_django(request):
     # seats =10
     # django_details ={'n':cname,'du':duration,'st':seats}
     # d= datetime.now
-    return render(request,'course/courseone.html',{'nm':'Django','st':5})
+    # student = {'names':['Rahul','Sonam','Raj','Anu']}
+
+    data ={'stu1':{'name':'Rahul','roll':101},
+          'stu2':{'name':'Sonam','roll':102},
+          'stu3':{'name':'Raj','roll':103},
+          'stu4':{'name':'Anu','roll':104},
+        }
+    # students = {'student':stu}  
+    return render(request,'course/courseone.html',{'data':data})
 def learn_python(request):
     return render(request,'course/coursetwo.html')
