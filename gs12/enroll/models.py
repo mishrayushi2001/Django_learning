83e316327f4c169baeac9cb172ab0e7b152c2857
mishrayushi2001 <mishrayushi2000@gmail.com>
2023-05-06 02:29:01 +0530

    show and retrive

--- gs12/enroll/models.py
+++ gs12/enroll/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
 class Student(models.Model):
-    stuid=models.IntegerField(primary_key=True)
+    stuid=models.IntegerField()
     stuname=models.CharField(max_length=70)
     stuemail=models.EmailField(max_length=70)
     stupass=models.CharField(max_length=70)
-    comment=models.CharField(max_length=70, default='not_available')
---
+++ gs12/enroll/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .import views
+
+yrlspatterns =[
+    path('stu/',views.studentinfo),
+]
