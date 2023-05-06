1b332a1b60909ee9db05f718192683dd6bd154d4
mishrayushi2001 <mishrayushi2000@gmail.com>
2023-05-06 18:53:07 +0530

    ModelAdmin Class

--- gs13/enroll/admin.py
+++ gs13/enroll/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from enroll.models import Student
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id','stuid','stuname','stuemail','stupass')
+
+# admin.site.register(Student,StudentAdmin)
--- gs13/enroll/models.py
+++ gs13/enroll/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    stuid=models.IntegerField()
+    stuname=models.CharField(max_length=70)
+    stuemail=models.EmailField(max_length=70)
+    stupass=models.CharField(max_length=70)
+
+    # def __str__(self): #__str__ method is used to display an object in Django admin site
+    #     return str(self.stuid)
+ 
