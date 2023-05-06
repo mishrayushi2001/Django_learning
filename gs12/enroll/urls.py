101b905abd987057a19275d441a3f41e73aabb66
mishrayushi2001 <mishrayushi2000@gmail.com>
2023-05-06 09:24:37 +0530

    fixed circular loop

--- gs12/enroll/urls.py
+++ gs12/enroll/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .import views
 
-yrlspatterns =[
+urlpatterns =[
     path('stu/',views.studentinfo),
 ]
--- gs12/enroll/views.py
+++ gs12/enroll/views.py
@@ -3,5 +3,6 @@ from enroll.models import Student
 
 def studentinfo(request):
     stud = Student.objects.all()
+    print('Myoutput',stud)
     return render(request,'enroll/studetails.html',{'stu':stud})
 
