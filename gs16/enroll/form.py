9a3358618e7b094ac18c6d607210178e1163f0fa
mishrayushi2001 <mishrayushi2000@gmail.com>
2023-05-09 22:34:43 +0530

    render form manually field and loop form field

---
+++ gs16/enroll/form.py
@@ -0,0 +1,8 @@
+from django import forms
+class StudentRegistration(forms.Form):
+    # name = forms.CharField(initial="Sonam", help_text="is field me 30 char")
+    name = forms.CharField()
+    email = forms.EmailField()
+    number = forms.IntegerField()
+
+    
