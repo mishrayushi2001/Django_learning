8fc900c2123975cf7c7de4ebc02cff4bdba65c8b
mishrayushi2001 <mishrayushi2000@gmail.com>
2023-05-16 13:08:25 +0530

    error field

--- gs22/enroll/form.py
+++ gs22/enroll/form.py
@@ -2,6 +2,8 @@
 from django.core import validators
 from django import forms
 class StudentRegistration(forms.Form):
+    error_css_class='error'
+    required_css_class='required'
     name = forms.CharField(error_messages={'required':'Enter Your Name'})
     email=forms.EmailField(error_messages={'required':'Enter Your Email'},min_length=5, max_length=50)
     password = forms.CharField(widget=forms.PasswordInput, error_messages={'required':'Enter Your password'},min_length=5, max_length=50)
