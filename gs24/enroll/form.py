0161cbe0bf3fa069d9f2c923809382c9b0d4299d
mishrayushi2001 <mishrayushi2000@gmail.com>
2023-05-19 12:56:47 +0530

    Model Form

--- gs24/enroll/form.py
+++ gs24/enroll/form.py
@@ -4,16 +4,18 @@ from django import forms
 from .models import User
 
 class StudentRegistration(forms.ModelForm):
+  # name = forms.CharField(max_length=50,required= False)
   class Meta:
     model=User 
     fields = ['name','email','password']
     labels = {'name':'Enter Name','password':'Enter password',
-              'email':'Enter Email',}
-    error_messages = {
-      'name':{'required':'Write Name'},
-      'password':{'required':'Write Password'},
-    }
-    widgets={'password':forms.PasswordInput,
-             'name':forms.TextInput(attrs={'class':'myclass','placeholder':'yaha name likhiye'})}
-
+    'email':'Enter Email',}
+    # error_messages = {
+    #   'name':{'required':'Write Name'},
+    #   'password':{'required':'Write Password'},
+    # }
+    # widgets={'password':forms.PasswordInput,
+    # 'name':forms.TextInput(attrs={'class':'myclass','placeholder':'yaha name likhiye'})}
+    widgets={'password':forms.PasswordInput}
+ 
  
--- gs24/enroll/views.py
+++ gs24/enroll/views.py
@@ -4,14 +4,18 @@ from .models import User
 
 def showformdata(request):
     if request.method =='POST':
-        fm=StudentRegistration(request.POST)
+        pi = User.objects.get()
+        fm=StudentRegistration(request.POST, instance=pi)
         if fm.is_valid():
-            nm = fm.cleaned_data['name']
-            em = fm.cleaned_data['email']
-            pw = fm.cleaned_data['password']
-            print(nm)
-            print(em)
-            print(pw)
+            fm.save()
+            # nm = fm.cleaned_data['name']
+            # em = fm.cleaned_data['email']
+            # pw = fm.cleaned_data['password']
+            # reg = User(name=nm,email=em,password=pw)
+            # reg.save()
+            # print(nm)
+            # print(em)
+            # print(pw)
     else:
         fm = StudentRegistration()  
         print("ye get request se aya h")        
